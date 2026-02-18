84b3964fc1e4e8a7a903296faece43fa117bafb2
Hizkyas Tadele <hizkyastadele@gmail.com>
2026-02-18 18:29:56 +0300

    Phase 2: Added dbt transformations and Metabase dashboarding

---
+++ dbt/models/weather_daily_summary.sql
@@ -0,0 +1,10 @@
+SELECT
+    date_trunc('hour', record_timestamp) as forecast_time,
+    count(*) as records_count,
+    ROUND(AVG(temp_celsius)::numeric, 2) as avg_temp,
+    MAX(temp_celsius) as max_temp,
+    MIN(temp_celsius) as min_temp,
+    ROUND(AVG(wind_speed_kmh)::numeric, 2) as avg_wind_speed
+FROM {{ ref('stg_weather') }}
+GROUP BY 1
+ORDER BY 1 DESC
---
+++ dbt/target/run/my_weather_project/models/weather_daily_summary.sql
@@ -0,0 +1,22 @@
+
+  
+    
+
+  create  table "airflow"."public"."weather_daily_summary__dbt_tmp"
+  
+  
+    as
+  
+  (
+    SELECT
+    date_trunc('hour', record_timestamp) as forecast_time,
+    count(*) as records_count,
+    ROUND(AVG(temp_celsius)::numeric, 2) as avg_temp,
+    MAX(temp_celsius) as max_temp,
+    MIN(temp_celsius) as min_temp,
+    ROUND(AVG(wind_speed_kmh)::numeric, 2) as avg_wind_speed
+FROM "airflow"."public_silver"."stg_weather"
+GROUP BY 1
+ORDER BY 1 DESC
+  );
+  
